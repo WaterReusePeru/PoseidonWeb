2af89f6d5e89bd638f3fa3455c64f472a503a5dd
Mirco Blaser <mirco.blaser@students.fhnw.ch>
2021-05-19 17:32:43 +0200

    Custom cell render in UnitProcesses

--- src/app/components/App.tsx
+++ src/app/components/App.tsx
@@ -5,12 +5,17 @@ import { ThemeSwitch } from '../../theme'
 import makeStyles from '@material-ui/core/styles/makeStyles'
 import UnitProcesses from './UnitProcessesMUI'
 import Typography from '@material-ui/core/Typography'
+import Tooltip from '@material-ui/core/Tooltip'
+import IconButton from '@material-ui/core/IconButton'
+import MenuBookIcon from '@material-ui/icons/MenuBook'
+import AddBoxIcon from '@material-ui/icons/AddBox'
+import ShowChartIcon from '@material-ui/icons/ShowChart'
 
 const useStyles = makeStyles({
   toolbar: {
     display: 'grid',
-    gridTemplateColumns: 'auto auto',
-    justifyItems: 'end',
+    gridTemplateColumns: 'auto 1fr auto',
+    justifyItems: 'center',
     justifyContent: 'space-between'
   },
   main: {
@@ -37,6 +42,23 @@ export const App = () => {
     <div className="App">
       <AppBar>
         <Toolbar className={classes.toolbar}>
+          <div>
+            <Tooltip title="Learn">
+              <IconButton color="inherit">
+                <MenuBookIcon />
+              </IconButton>
+            </Tooltip>
+            <Tooltip title="New Case">
+              <IconButton color="inherit">
+                <AddBoxIcon />
+              </IconButton>
+            </Tooltip>
+            <Tooltip title="View Results">
+              <IconButton color="default">
+                <ShowChartIcon />
+              </IconButton>
+            </Tooltip>
+          </div>
           <div className={classes.title}>
             <Typography>Poseidon Web alpha 0.1</Typography>
           </div>
--- src/app/components/UnitProcessesMUI.js
+++ src/app/components/UnitProcessesMUI.js
@@ -3,6 +3,8 @@ import { DataGrid } from '@material-ui/data-grid'
 import { makeStyles } from '@material-ui/core/styles'
 
 import unitProcesses from '../data/unitProcesses.json'
+import Chip from '@material-ui/core/Chip'
+import Tooltip from '@material-ui/core/Tooltip'
 
 const useStyles = makeStyles({
   root: {
@@ -15,114 +17,147 @@ const useStyles = makeStyles({
     '& .MuiDataGrid-columnsContainer': {
       lineHeight: 'inherit'
     }, */
+  },
+  chipContainer: {
+    display: 'flex',
+    justifyContent: 'center',
+    flexWrap: 'wrap',
+    '& > *': {
+      margin: 2
+    }
   }
 })
 
 const data = unitProcesses
 
-const columns = [
-  {
-    field: 'id',
-    headerName: 'ID'
-  },
-  {
-    field: 'name',
-    headerName: 'Name',
-    width: 200
-  },
-  {
-    field: 'turbity',
-    headerName: 'Turbidity [% removal]',
-    flex: 1,
-    headerClassName: 'wrapperHeader'
-  },
-  {
-    field: 'tss',
-    headerName: 'TSS [% removal]',
-    flex: 1
-  },
-  {
-    field: 'bod',
-    headerName: 'BOD [% removal]',
-    flex: 1
-  },
-  {
-    field: 'cod',
-    headerName: 'COD [% removal]',
-    flex: 1
-  },
-  {
-    field: 'fc',
-    headerName: 'FC [% removal]',
-    flex: 1
-  },
-  {
-    field: 'tc',
-    headerName: 'TC [% removal]',
-    flex: 1
-  },
-  /* {
-    field: "construction_cost_b",
-    headerName: "const cost b",
-  },
-  {
-    field: "construction_cost_c",
-    headerName: "const cost c",
-  },
-  {
-    field: "land_requirements_b",
-    headerName: "land req b",
-  },
-  {
-    field: "land_requirements_c",
-    headerName: "land req c",
-  },
-  {
-    field: "energy_requirements_b",
-    headerName: "energy req b",
-  },
-  {
-    field: "energy_requirements_c",
-    headerName: "energy req c",
-  },
-  {
-    field: "labor_requirements_b",
-    headerName: "labor req b",
-  },
-  {
-    field: "labor_requirements_c",
-    headerName: "labor req c",
-  },
-  {
-    field: "other_om_b",
-    headerName: "other om b",
-  },
-  {
-    field: "other_om_c",
-    headerName: "other om c",
-  }, */
-  {
-    field: 'recovery',
-    headerName: 'Recovery [%]',
-    flex: 1
-  },
-  {
-    field: 'reliability',
-    headerName: 'Reliability',
-    flex: 1
-  },
-  {
-    field: 'ease_to_upgrade',
-    headerName: 'Ease to Upgrade'
-  }
-]
-
 export default function UnitProcesses() {
   const classes = useStyles()
 
+  function getPollutantRemovalEfficiencies(params) {
+    console.log(params)
+
+    const columns = ['turbidity', 'tss', 'bod', 'cod', 'fc', 'tc']
+    const columnTitles = ['Turbidity', 'TSS', 'BOD', 'COD', 'FC', 'TC']
+
+    return (
+      <div className={classes.chipContainer}>
+        {columns.map((column, index) => (
+          <Tooltip title={columnTitles[index]}>
+            <Chip label={params.getValue(column)} key={index} size="small" />
+          </Tooltip>
+        ))}
+      </div>
+    )
+  }
+
+  const columns = [
+    {
+      field: 'id',
+      headerName: 'ID'
+    },
+    {
+      field: 'name',
+      headerName: 'Name',
+      width: 200
+    },
+    {
+      field: 'turbidity',
+      headerName: 'Pollutant Removal Efficiencies [% removal]',
+      flex: 1,
+      //headerClassName: 'wrapperHeader'
+      renderCell: params => {
+        return getPollutantRemovalEfficiencies(params)
+      }
+    },
+    /*
+    {
+      field: 'tss',
+      headerName: 'TSS [% removal]',
+      flex: 1
+    },
+     {
+      field: 'bod',
+      headerName: 'BOD [% removal]',
+      flex: 1
+    },
+    {
+      field: 'cod',
+      headerName: 'COD [% removal]',
+      flex: 1
+    },
+    {
+      field: 'fc',
+      headerName: 'FC [% removal]',
+      flex: 1
+    },
+    {
+      field: 'tc',
+      headerName: 'TC [% removal]',
+      flex: 1
+    },
+    {
+      field: "construction_cost_b",
+      headerName: "const cost b",
+    },
+    {
+      field: "construction_cost_c",
+      headerName: "const cost c",
+    },
+    {
+      field: "land_requirements_b",
+      headerName: "land req b",
+    },
+    {
+      field: "land_requirements_c",
+      headerName: "land req c",
+    },
+    {
+      field: "energy_requirements_b",
+      headerName: "energy req b",
+    },
+    {
+      field: "energy_requirements_c",
+      headerName: "energy req c",
+    },
+    {
+      field: "labor_requirements_b",
+      headerName: "labor req b",
+    },
+    {
+      field: "labor_requirements_c",
+      headerName: "labor req c",
+    },
+    {
+      field: "other_om_b",
+      headerName: "other om b",
+    },
+    {
+      field: "other_om_c",
+      headerName: "other om c",
+    }, */
+    {
+      field: 'recovery',
+      headerName: 'Recovery [%]',
+      flex: 0.5
+    },
+    {
+      field: 'reliability',
+      headerName: 'Reliability',
+      flex: 1
+    },
+    {
+      field: 'ease_to_upgrade',
+      headerName: 'Ease to Upgrade'
+    },
+    {
+      field: 'ease_to_upgrade',
+      headerName: 'Evaluation'
+    }
+  ]
+
   return (
     <div style={{ flexGrow: 1 }} className={classes.root}>
-      <DataGrid rows={data} columns={columns} pageSize={20} headerHeight={120} />
+      <DataGrid rows={data} columns={columns} pageSize={10} headerHeight={120} />
     </div>
   )
 }
