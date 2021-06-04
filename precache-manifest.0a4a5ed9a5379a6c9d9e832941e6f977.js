85b4637fad228147feb8a20ff682c8c41e985e5f
denacem <denacem@users.noreply.github.com>
2021-06-04 09:23:28 +0000

    deploy: 10145a74dad5e743ad285de5790ed061b0e138db

---
+++ precache-manifest.0a4a5ed9a5379a6c9d9e832941e6f977.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "358a4c94961ee68dae705e009ac278cc",
+    "url": "/PoseidonWeb/index.html"
+  },
+  {
+    "revision": "07ae014a2a5667951f32",
+    "url": "/PoseidonWeb/static/js/2.d50f854c.chunk.js"
+  },
+  {
+    "revision": "3da521dbb4874350fb1d5ccbd1bab881",
+    "url": "/PoseidonWeb/static/js/2.d50f854c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d7af12e269a7f208141",
+    "url": "/PoseidonWeb/static/js/main.da9076da.chunk.js"
+  },
+  {
+    "revision": "5f048fca55f5cf5e823e",
+    "url": "/PoseidonWeb/static/js/runtime-main.3114d419.js"
+  }
+]);
