3f3b78779d304fcbfabecfdb2a3927c31da34e50
denacem <denacem@users.noreply.github.com>
2021-07-04 20:57:16 +0000

    deploy: 0242f62aacfc4e3d4706496f16e30af6f7072fb0

---
+++ precache-manifest.bb499619366fc835319b11024f6bd36a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfbc30ffeff7f35bf30ef2cdedd0ae2e",
+    "url": "/PoseidonWeb/index.html"
+  },
+  {
+    "revision": "026558d2fe7f7a74012e",
+    "url": "/PoseidonWeb/static/js/2.35b0df44.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/PoseidonWeb/static/js/2.35b0df44.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a6574f1c419a39a5bc8",
+    "url": "/PoseidonWeb/static/js/main.322eb86e.chunk.js"
+  },
+  {
+    "revision": "5f048fca55f5cf5e823e",
+    "url": "/PoseidonWeb/static/js/runtime-main.3114d419.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/PoseidonWeb/precache-manifest.35e8610f8d233687e90b864fdcdcdbb3.js"
+  "/PoseidonWeb/precache-manifest.bb499619366fc835319b11024f6bd36a.js"
 );
 
 self.addEventListener('message', (event) => {
